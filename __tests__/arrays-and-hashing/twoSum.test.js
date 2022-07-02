f501ba2ba74b9866988835940662872384e072c4
Tyler Holt <tylerprestonholt@gmail.com>
2022-07-01 23:11:07 -0400

    add tests for twoSum

--- __tests__/arrays-and-hashing/twoSum.test.js
+++ __tests__/arrays-and-hashing/twoSum.test.js
@@ -1,3 +1,30 @@
 import { twoSum } from "../../solutions/arrays-and-hashing/twoSum";
 
-describe("testing twoSum", () => {});
+describe("testing twoSum", () => {
+  let nums;
+  let target;
+
+  it("should error when first input is not an array", () => {
+    nums = {};
+    target = 0;
+    expect(() => twoSum(nums, target)).toThrow(Error);
+  });
+
+  it("should error when second input is not a number", () => {
+    nums = [];
+    target = "0";
+    expect(() => twoSum(nums, target)).toThrow(Error);
+  });
+
+  it("should return false when no two addends sum to target", () => {
+    nums = [1, 2, 3, 4, 5];
+    target = 10;
+    expect(twoSum(nums, target)).toBe(false);
+  });
+
+  it("should return true when two addends sum to target", () => {
+    nums = [1, 2, 3, 4, 5];
+    target = 6;
+    expect(twoSum(nums, target)).toBe(true);
+  });
+});
